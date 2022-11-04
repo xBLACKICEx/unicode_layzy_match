fa44b7c3a50839ebff172ca41cbc754f4589e709
xblakicex <xblakicex@outlook.com>
2022-11-04 20:29:58 +0100

    🦄 refactor:  code and remove unnecessary code.

--- src/lib.rs
+++ src/lib.rs
@@ -15,76 +15,91 @@ use deunicode::deunicode;
 ///   "彼方",     // bi fang
 ///   "其他",     // qi ta
 ///   ];
-/// 
+///
 /// assert_eq!(vec!["よろしく"], unicode_layzy_match(&lst_uc, "y").unwrap());
 /// assert_eq!(vec!["こんにちわ"], unicode_layzy_match(&lst_uc, "kon").unwrap());
 /// assert_eq!(vec!["北方", "彼方"], unicode_layzy_match(&lst_uc, "bf").unwrap());
 /// ```
 pub fn unicode_layzy_match<'a>(lst_uc: &Vec<&'a str>, usr_ipt: &str) -> Option<Vec<&'a str>> {
-    if let Some(lst_uc_ascii) = unicde_first_ascii_char_mached(&lst_uc, usr_ipt) {
-        if let Some(matched_lzy_uc) = uncode_layzy_pinyin_matched(&lst_uc_ascii, usr_ipt, &lst_uc) {
-            return Some(matched_lzy_uc);
+    if let Some((mut lst_ps, mut lst_uc)) = unicde_first_ascii_char_mached(&lst_uc, usr_ipt) {
+        let matched_lzy_py = uncode_layzy_pinyin_matched(&mut lst_ps, &mut lst_uc, usr_ipt);
+
+        if let Some(mut matched_uc) = unicode_ascii_matched(&mut lst_ps, &mut lst_uc, usr_ipt) {
+            if let Some((_, mut lz_py)) = matched_lzy_py {
+                lz_py.append(&mut matched_uc.1);
+                return Some(lz_py);
+            }
+            return Some(matched_uc.1);
+        } else if let Some((_, hz)) = matched_lzy_py {
+            return Some(hz);
         }
-        return unicode_ascii_matched(&lst_uc_ascii, usr_ipt, &lst_uc);
-    };
+    }
     None
 }
 
-fn unicde_first_ascii_char_mached(
-    lst_uc: &Vec<&str>,
+fn unicde_first_ascii_char_mached<'a>(
+    lst_uc: &Vec<&'a str>,
     usr_ipt: &str,
-) -> Option<Vec<(String, usize)>> {
-    let mut lst_uni_ascii: Vec<(String, usize)> = Vec::new();
+) -> Option<(Vec<String>, Vec<&'a str>)> {
+    let mut lst_uni_ascii: (Vec<String>, Vec<&'a str>) = (Vec::new(), Vec::new());
 
-    lst_uc.iter().enumerate().for_each(|(i, hz)| {
+    lst_uc.iter().for_each(|hz| {
         let tmp = deunicode(hz).to_lowercase();
         if tmp.starts_with(&usr_ipt[0..1]) {
-            lst_uni_ascii.push((tmp, i));
+            lst_uni_ascii.0.push(tmp);
+            lst_uni_ascii.1.push(hz);
         }
     });
-    if !lst_uni_ascii.is_empty() {
+    if !lst_uni_ascii.0.is_empty() {
         return Some(lst_uni_ascii);
     }
     None
 }
 
-fn uncode_layzy_pinyin_matched<'a>(
-    lst_uni_ascii: &Vec<(String, usize)>,
+fn unicode_ascii_matched<'a>(
+    lst_ps: &mut Vec<String>,
+    lst_uc: &mut Vec<&'a str>,
     usr_ipt: &str,
-    lst_uc: &Vec<&'a str>,
-) -> Option<Vec<&'a str>> {
-    let lazy_py: Vec<&(String, usize)> = lst_uni_ascii
-        .iter()
-        .filter(|(h, _)| match_layzy_pinyin(h.split_whitespace().collect(), usr_ipt))
-        .collect();
+) -> Option<(Vec<String>, Vec<&'a str>)> {
+    let mut mached_py = vec![];
+    let mut mached_hz = vec![];
 
-    if !lazy_py.is_empty() {
-        let l: Vec<_> = lazy_py.iter().map(|(_, i)| lst_uc[*i]).collect();
-        return Some(l);
+    let mut i = 0;
+    while i < lst_ps.len() {
+        if lst_ps[i].to_lowercase().starts_with(usr_ipt) {
+            mached_py.push(lst_ps.remove(i));
+            mached_hz.push(lst_uc.remove(i));
+        } else {
+            i += 1;
+        }
+    }
+
+    if !mached_py.is_empty() {
+        return Some((mached_py, mached_hz));
     }
     None
 }
 
-fn unicode_ascii_matched<'a>(
-    lst_uc_ascii: &Vec<(String, usize)>,
+fn uncode_layzy_pinyin_matched<'a>(
+    lst_py: &mut Vec<String>,
+    lst_hz: &mut Vec<&'a str>,
     usr_ipt: &str,
-    lst_uc: &Vec<&'a str>,
-) -> Option<Vec<&'a str>> {
-    let hz_py: Vec<&(String, usize)> = lst_uc_ascii
-        .iter()
-        .filter(|(h, _)| {
-            let h = h
-                .chars()
-                .filter(|c| !c.is_whitespace())
-                .collect::<String>()
-                .as_str()
-                .to_lowercase();
-            h.chars().zip(usr_ipt.chars()).all(|(s1, s2)| s1 == s2)
-        })
-        .collect();
-    if !hz_py.is_empty() {
-        let l: Vec<_> = hz_py.iter().map(|(_, i)| lst_uc[*i]).collect();
-        return Some(l);
+) -> Option<(Vec<String>, Vec<&'a str>)> {
+    let mut mached_py = vec![];
+    let mut mached_hz = vec![];
+
+    let mut i = 0;
+    while i < lst_py.len() {
+        if match_layzy_pinyin(lst_py[i].to_lowercase().split_whitespace().collect(), usr_ipt) {
+            mached_py.push(lst_py.remove(i));
+            mached_hz.push(lst_hz.remove(i));
+        } else {
+            i += 1;
+        }
+    }
+
+    if !mached_py.is_empty() {
+        return Some((mached_py, mached_hz));
     }
     None
 }
